6eca09c85d7babab0143aeef603a840b8ea751c3
yunolva <yunolvazy@gmail.com>
2023-04-06 06:37:59 +0700

    Progress 5th

---
+++ ez-pz/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  // saat tombol plus di klik
+  $('.btn-plus').click(function(){
+      // ambil nilai input
+      var value = parseInt($('input[name="quantity"]').val());
+      // tambahkan 1 ke nilai input
+      value = isNaN(value) ? 0 : value;
+      value++;
+      $('input[name="quantity"]').val(value);
+  });
+  
+  // saat tombol minus di klik
+  $('.btn-minus').click(function(){
+      // ambil nilai input
+      var value = parseInt($('input[name="quantity"]').val());
+      // kurangi 1 dari nilai input
+      value = isNaN(value) ? 0 : value;
+      if(value > 1){
+          value--;
+      }
+      $('input[name="quantity"]').val(value);
+  });
+});
